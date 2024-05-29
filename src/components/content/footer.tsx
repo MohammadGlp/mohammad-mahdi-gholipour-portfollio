4f5a93cd100c602cb319141d4cb194fe38c0e972
MohammadGlp <90711516+MohammadGlp@users.noreply.github.com>
2024-05-29 19:33:03 +0330

    add footer

---
+++ src/components/content/footer.tsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <footer>
+      <div className="flex items-center justify-between font-bellefair sm:text-2xl text-sm text-secondary md:py-10 py-8">
+        <h3
+          className="hover:text-primary ease-in-out duration-300 cursor-pointer"
+          onClick={() => handleScrollToTop()}
+        >
+          back ↑
+        </h3>
+        <p>© Mohammad Gholipour, 2024</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
